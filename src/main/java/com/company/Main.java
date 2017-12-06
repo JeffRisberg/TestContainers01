a0cfc197e93d0331fb9c89233c94f4a531bdb08f
JeffRisberg <jrisberg@aol.com>
2017-12-05 18:48:42 -0800

    added logging

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
 import com.company.models.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     protected List<User> users;
 
@@ -26,6 +29,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        LOGGER.debug("Welcome to Example 1");
+
         Main main = new Main();
 
         User user1 = new User(1l, "Jack", "Jones");
