dd8d65b60b4d567fb42792ad77e66ff317e46d25
JeffRisberg <jrisberg@aol.com>
2018-11-23 23:59:09 -0800

    added item service test

--- src/test/java/com/company/ItemServiceTest.java
+++ src/test/java/com/company/ItemServiceTest.java
@@ -9,7 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -28,22 +32,43 @@ public class ItemServiceTest {
 
   @Test
   public void getItemNameUpperCase() {
-
     //
     // Given
     //
-    Item mockedItem = new Item(22L, "Item 1", "This is item 1", 2000);
-    when(itemStore.findById(22L)).thenReturn(mockedItem);
+    Item mockedItem = new Item(1L, "Item 1", "This is item 1", 2000);
+    when(itemStore.findById(1L)).thenReturn(mockedItem);
 
     //
     // When
     //
-    String result = itemService.getItemNameUpperCase(22L);
+    String result = itemService.getItemNameUpperCase(1L);
 
     //
     // Then
     //
-    verify(itemStore, times(1)).findById(22L);
+    verify(itemStore, times(1)).findById(1L);
     assertThat(result, is("ITEM 1"));
+  }
+
+  @Test
+  public void getAveragePrice() {
+    //
+    // Given
+    //
+    Item mockedItem1 = new Item(1L, "Item 1", "This is item 1", 2000);
+    Item mockedItem2 = new Item(2L, "Item 2", "This is item 2", 4000);
+    List<Item> mockedItems = new ArrayList<>();
+    mockedItems.add(mockedItem1);
+    mockedItems.add(mockedItem2);
+
+    when(itemStore.readAllItems()).thenReturn(mockedItems);
+
+    //
+    // When
+    //
+    int result = itemService.getAveragePrice();
+
+    verify(itemStore, times(1)).readAllItems();
+    assertEquals(3000, result);
   }
 }
