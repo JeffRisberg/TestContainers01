de085cb0ba6658a42b509a17a7e4118fde9df96a
JeffRisberg <jrisberg@aol.com>
2018-12-07 20:09:21 -0800

    refined one test

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,12 +1,6 @@
 package com.company;
 
-import com.company.models.Item;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 public class Main {
