7ce5e852ca8426c16e3460f44b858c1a96f9c488
JeffRisberg <j.s.risberg@gmail.com>
2021-04-06 23:46:40 -0700

    first commit

--- src/main/java/com/company/RedisBackedCacheIntTestStep0.java
+++ src/main/java/com/company/RedisBackedCacheIntTestStep0.java
@@ -4,6 +4,10 @@ package com.company;
 public class RedisBackedCacheIntTestStep0 {
   private RedisBackedCache underTest;
 
+  @Rule
+  public GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+      .withExposedPorts(6379);
+
   @Before
   public void setUp() {
     // Assume that we have Redis running locally?
