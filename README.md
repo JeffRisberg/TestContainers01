ac9ca2906acb17daf9a14a35010125b95344b713
JeffRisberg <j.s.risberg@gmail.com>
2021-04-06 23:43:30 -0700

    first commit

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# Example01
+# TestContainer01
 
-Example of using Gradle for Java project.
-
-Also an example of using JUnit 4.13
-
-This is also a test of using webhooks between GitHub and Jenkins.
-
-Updated the webhook in September 2020.
-
+Example of using TestContainers library.
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+//@Slf4j
 public class Main {
 
   public static void main(String[] args) {
-    log.info("Welcome to Example 1");
+    //log.info("Welcome to TestContainers01");
+    //test
   }
 }
